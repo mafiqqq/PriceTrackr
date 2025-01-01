f68f5e1ad2136632821c145bcae9bcb2950f02c5
Mohamed Afiq <afiqqqx1997@gmail.com>
2025-01-01 13:41:56 +0800

    init project

---
+++ PriceTrackrAPI/Model/Login.cs
@@ -0,0 +1,8 @@
+﻿namespace PriceTrackrAPI.Model
+{
+    public class Login
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
